6084033338d341295c507d156cac104c11af728e
Chau Corn <baochau271195@gmail.com>
2024-04-02 14:56:58 +0200

    20.1-3

--- Chap_20/20.1-3.py
+++ Chap_20/20.1-3.py
@@ -1,3 +1,4 @@
+#Adjacency list representation of a graph and its reverse graph
 # Number of vertices
 n = 4
 
@@ -27,7 +28,39 @@ def reverse_graph(graph):
         for neighbor in neighbors:
             reverse_graph[neighbor].append(i)
     return reverse_graph
+print("Original graph:")
 print_graph(graph)
+print("Reverse graph:")
 reverse_graph = reverse_graph(graph)
 print_graph(reverse_graph)
+print("Complexity: O(V+E)")
+
+# Adjacency matrix representation of a graph and its reverse graph
+
+n = 4
+# Create a matrix
+matrix = [[0 for _ in range(n)] for _ in range(n)]
+matrix[0][1] = 1
+matrix[0][2] = 1
+matrix[1][2] = 1
+matrix[2][0] = 1
+matrix[2][3] = 1
+matrix[3][3] = 1
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+def reverse_matrix(matrix):
+    reverse_matrix = [[0 for _ in range(n)] for _ in range(len(matrix))]
+    for i in range (len(matrix)):
+        for j in range (len(matrix)):
+            if matrix[i][j] == 1:
+                reverse_matrix[j][i] = 1
+    return reverse_matrix
+print("Original matrix:")
+print_matrix(matrix)
+print("Reverse matrix:")
+print_matrix(reverse_matrix(matrix))
+print("Complexity: O(V^2)")
 
